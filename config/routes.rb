a5bcc6304246deeeb91acb2faf80165c7c7e7bc2
Aurelio Tinio <aurelio@tinio.org>
2012-04-10 09:06:46 -0700

    route route to home index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Preparedly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
