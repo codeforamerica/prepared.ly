07c539d8c097747365e5ad0a8c75fa23f0988813
Joe Merante <merante.joe@gmail.com>
2012-04-16 18:08:49 -0700

    Clean some comments from auth files

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  def twitter
-    # p request.env["omniauth.auth"]
-
-    # You need to implement the method below in your model
     user = current_user || User.find_for_twitter_oauth(request.env["omniauth.auth"])
     sign_in_and_redirect user, :event => :authentication
 
