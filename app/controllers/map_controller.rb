10b817095c45f036094a54982dddac3dde85a272
Aurelio Tinio <aurelio@tinio.org>
2012-04-17 11:19:01 -0700

    [#27955903]display weather conditions

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,11 @@
+require 'wunderground'
+
 class MapController < ApplicationController
   def index
     @address = params[:q]
     @coordinates = Geocoder.coordinates(@address)
+    w_api = Wunderground.new(ENV['WUNDERGROUND_API_KEY'])
+    w_response = w_api.get_forecast_for(@coordinates[0].to_s + "," + @coordinates[1].to_s)
+    @weather = w_response['forecast']['txt_forecast']['forecastday'][0]['fcttext']
   end
 end
