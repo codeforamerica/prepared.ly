139b06633f1dc6c451b852f800ab753979ca12c8
Aurelio Tinio <aurelio@tinio.org>
2012-04-17 13:01:10 -0700

    [delivers #27955903] display just wind conditions and humidity

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,7 +5,8 @@ class MapController < ApplicationController
     @address = params[:q]
     @coordinates = Geocoder.coordinates(@address)
     w_api = Wunderground.new(ENV['WUNDERGROUND_API_KEY'])
-    w_response = w_api.get_forecast_for(@coordinates[0].to_s + "," + @coordinates[1].to_s)
-    @weather = w_response['forecast']['txt_forecast']['forecastday'][0]['fcttext']
+    w_response = w_api.get_conditions_for(@coordinates[0].to_s + "," + @coordinates[1].to_s)
+    @wind_conditions = w_response['current_observation']['wind_string']
+    @relative_humidity = w_response['current_observation']['relative_humidity']
   end
 end
