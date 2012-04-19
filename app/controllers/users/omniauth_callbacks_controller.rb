accb2419f01fd74f06b4b13e64024d83a83f7d56
Joe Merante <merante.joe@gmail.com>
2012-04-18 17:49:03 -0700

    Work on third party sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,14 +13,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   end 
   end
 
-  def google
+   def google
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.open:id_data"] = request.env["openid.ext1"]
       redirect_to new_user_registration_url
     end
   end
