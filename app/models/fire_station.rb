30642b97073c7eac40f642df34d9288ab415cc96
Aurelio Tinio <aurelio@tinio.org>
2012-04-26 17:49:28 -0700

    FireStation.populate runner script

--- app/models/fire_station.rb
+++ app/models/fire_station.rb
@@ -1,3 +1,22 @@
+require 'csv'
+require 'open-uri'
+
 class FireStation < ActiveRecord::Base
   attr_accessible :address, :latlon, :zip
+
+  def self.populate
+    csv_text = nil
+    open('https://raw.github.com/gist/2504610/76e47ca52136f2d6a88585cf8755e887795ee0ab/afd_stations.csv') do |f|
+      csv_text = f.read()
+    end
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      fs = FireStation.create
+      fs.address = row[2]
+      fs.zip = row[5].to_i
+      fs.latlon = "POINT(" + row[7] + " " + row[6] +")"
+      puts fs.address, fs.zip, fs.latlon
+      fs.save!
+    end
+  end
 end
