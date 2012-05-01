1406f3812f7da7163a78f9c793e321bfbc1d3353
Aurelio Tinio <aurelio@tinio.org>
2012-05-01 15:33:22 -0700

    formatting

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -13,16 +13,19 @@ class MapController < ApplicationController
 
     # Closest Fire Station
     @cfs = FireStation.order("ST_Distance(latlon, '" + @address.latlon.to_s + "') LIMIT 1")[0]
-    @distance = @address.latlon.distance(@cfs.latlon)
+    @distance = "%.02f" % @address.latlon.distance(@cfs.latlon)
 
+    # Weather Conditions
     w_api = Wunderground.new(ENV['WUNDERGROUND_API_KEY'])
     w_response = w_api.get_conditions_for(@address.latlon.y.to_s + "," + @address.latlon.x.to_s)
     @wind_conditions = w_response['current_observation']['wind_string']
     @relative_humidity = w_response['current_observation']['relative_humidity']
 
+    # Counties with a Burn Ban
     rss = Nokogiri::XML(open('http://tfsfrp.tamu.edu/wildfires/BurnBan.xml'))
     rss.encoding = 'utf-8'
     counties = rss.css('rss channel item description').text
     @counties_list = '\'' + counties.strip.split(', ').join("\', \'") + '\''
+
   end
 end
