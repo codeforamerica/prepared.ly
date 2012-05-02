c3d6761b90b9644e75b63095b95641aaab6862c9
Aurelio Tinio <aurelio@tinio.org>
2012-05-02 06:55:43 -0700

    change county geometry to be multipolygon

--- db/migrate/20120502010306_create_counties.rb
+++ db/migrate/20120502010306_create_counties.rb
@@ -2,7 +2,7 @@ class CreateCounties < ActiveRecord::Migration
   def change
     create_table :counties do |t|
       t.string :name
-      t.polygon :geometry, :geographic => true
+      t.multi_polygon :geometry, :geographic => true
 
       t.timestamps
     end
