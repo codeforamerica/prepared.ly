7f578135815c22d068492cc1aa8b41b1505b4d1a
Emily Wright <emily@emilyville.com>
2012-05-03 15:11:45 -0700

    authbuttons

--- app/helpers/authentications_helper.rb
+++ app/helpers/authentications_helper.rb
@@ -1,2 +1,12 @@
 module AuthenticationsHelper
-end
+ def provider_icon(provider, size=32)
+   path = "authbuttons/#{provider}_#{size}.png"
+   provider_name = OmniAuth::Utils.camelize(provider)
+
+   if File.exists?(Rails.root.join('app', 'assets', 'images', path))
+     image_tag(path, :alt => provider_name)
+   else
+     provider_name
+   end
+  end
+ end
