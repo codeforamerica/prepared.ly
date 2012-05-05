b653b46ede8d85366464a5d7812db5066b1e7533
Joe Merante <merante.joe@gmail.com>
2012-05-04 17:17:42 -0700

    Get welcome email working for non-Twitter signups

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     def create
-        super()
-        current_user.referral_code = request.env['affiliate.tag']
-        current_user.save!
-    end
-
-    def after_sign_up_path_for(resource)
-      @user = current_user
+      super()
+      current_user.referral_code = request.env['affiliate.tag']
+      current_user.save!
       unless @user.email == "change@changeme.com"
       	@user.send_welcome_email
       end
+    end
+
+    def after_sign_up_path_for(resource)
       "/share"
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,8 @@ class User < ActiveRecord::Base
     end
   end
 
+ # after_create :send_welcome_email
+
    def send_welcome_email
       UserMailer.welcome_email(self).deliver
    end 
