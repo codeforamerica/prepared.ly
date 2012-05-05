a2bba4552df9167abb03780e8018b580b53fd3d5
Joe Merante <merante.joe@gmail.com>
2012-05-04 18:11:56 -0700

    Send welcome email when Twitter update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,13 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user = current_user	
+		@user = current_user
+		prev_email = @user.email
 		@user.update_attributes(params[:user])
+		# send welcome email to user who signs up with Twitter then updates their email
+		if prev_email == "change@changeme.com" && @user.email != "change@changeme.com" 
+			@user.send_welcome_email
+		end
 		redirect_to share_path, :flash => { :success => "Profile updated." }
 	end
 end
