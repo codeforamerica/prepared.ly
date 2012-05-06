50d1dbf2369b8e5ec016aa7a53ea96946e527416
Joe Merante <merante.joe@gmail.com>
2012-05-06 15:20:35 -0700

    Entire relative path to welcome email logo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
   	@user 				= user
   	@url 					= "http://prepared.ly/users/sign_in"
   	@referral_url	= "http://www.prepared.ly/?ref=<%= @referral_code %>"
-  	attachments.inline['preparedly-red.png'] = File.read('preparedly-red.png')
+  	attachments.inline['preparedly-red.png'] = File.read('app/assets/images/preparedly-red.png')
   	mail(:to => user.email, :subject => "Welcome to Prepared.ly")
   end
 end
