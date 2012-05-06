504eeb6a9245ca9ede6be4e58aec166c81f2c5f8
Joe Merante <merante.joe@gmail.com>
2012-05-06 00:10:45 -0700

    Update yahoo oauth request scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -223,8 +223,8 @@ Devise.setup do |config|
   config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
   require 'omniauth-yahoo'
-  config.omniauth :yahoo, ENV['YAHOO_ID'], ENV['YAHOO_SECRET']
-  
+  config.omniauth :yahoo, ENV['YAHOO_ID'], ENV['YAHOO_SECRET'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
