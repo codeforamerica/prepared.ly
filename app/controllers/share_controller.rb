2b5187cf93340b332875d10ba2d7be3b5c1638bf
Aurelio Tinio <aurelio@tinio.org>
2012-05-07 10:50:03 -0700

    save share_code in user model

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -2,5 +2,7 @@ class ShareController < ApplicationController
     def index
         referral_id = 123456789 + current_user.id
         @referral_code = referral_id.to_s(36) 
+        current_user.share_code = @referral_code
+        current_user.save!
     end
 end
