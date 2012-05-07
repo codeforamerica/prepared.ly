be7e2946ae06fcc7eac11e7fd2a2362f8216155f
Aurelio Tinio <aurelio@tinio.org>
2012-05-07 14:14:14 -0700

    [delivers #28968355]set up message sending

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,18 @@
+require 'twilio-ruby'
+
 class Message < ActiveRecord::Base
   attr_accessible :body
+
+  def send_message
+    @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    User.find(:all, :conditions => "phone != ''").each do |user|
+      @client.account.sms.messages.create(
+        :from => '+15128616101',
+        :to => user.phone,
+        :body => self.body
+      )
+    end
+  end
+
+  after_create :send_message
 end
