67250a26d09996890bf6dc3ce36aaaacb43b71ef
Aurelio Tinio <aurelio@tinio.org>
2012-05-08 19:29:23 -0700

    closes #11 display if no risk data is found

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -22,7 +22,11 @@ class TFS
       }
     )
     json_response = JSON.parse(response.body)
-    return json_response['results'][0]['attributes']['Pixel Value'].to_i
+    if json_response['results'].length > 0
+      return json_response['results'][0]['attributes']['Pixel Value'].to_i
+    else
+      return nil
+    end
   end
 end
 
