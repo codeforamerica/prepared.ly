1cdac35c6d0419243d1eec3e0c7c1a67be09769a
Aurelio Tinio <aurelio@tinio.org>
2012-05-08 17:55:22 -0700

    change default landing page to display map

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Preparedly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'map#get'
 
   # See how all your routes lay out with "rake routes"
 
