1870432e64b997b076011eebd16ca322d728e2ab
Joe Merante <merante.joe@gmail.com>
2012-05-09 14:22:59 -0700

    Getting warmer..

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,10 +4,10 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
   	@user 					= user
   	@url 						= "http://prepared.ly/users/sign_in"
-  	@referral_url		= "http://www.prepared.ly/?ref#{@user.referral_code}"
-  	@facebook_share = "http://www.facebook.com/sharer.php?u=#{@user.referral_code}"
-  	@tumblr_share 	= "http://www.tumblr.com/share/link?url=#{@user.referral_code}"
-  	@twitter_share 	= "http://twitter.com/share?text=Check%20out%20Preparedly&url=#{@user.referral_code}"
+  	@referral_url		= "http://www.prepared.ly/?ref<%= @user.referral_code %>"
+  	@facebook_share = "http://www.facebook.com/sharer.php?u=<%= @user.referral_code %>"
+  	@tumblr_share 	= "http://www.tumblr.com/share/link?url=<%= @user.referral_code %>"
+  	@twitter_share 	= "http://twitter.com/share?text=Check%20out%20Preparedly&url=<%= @user.referral_code %>"
   	@survey 				= "http://bit.ly/preparedlysurvey"
   	attachments.inline['preparedly-red.png'] = File.read('app/assets/images/preparedly-red.png')
   	mail(:to => user.email, :subject => "Welcome to Prepared.ly")
