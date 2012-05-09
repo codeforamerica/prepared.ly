d8d4f67a4368e7b1feae0c740863eae1f51e663b
Joe Merante <merante.joe@gmail.com>
2012-05-09 01:04:56 -0700

    Fixed text version of welcome email and logic in users controller

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -2,7 +2,8 @@ class ShareController < ApplicationController
     def index
         referral_id = 123456789 + current_user.id
         @referral_code = referral_id.to_s(36) 
-        current_user.share_code = @referral_code
+        # not sure why share_code throws an error here - need to migrate db maybe?
+        # current_user.share_code = @referral_code
         current_user.save!
     end
 end
