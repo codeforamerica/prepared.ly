9b543444ac5e92b838648d6ea931d01ff8e9b596
Joe Merante <merante.joe@gmail.com>
2012-05-09 15:31:42 -0700

    no more nil..

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -2,8 +2,7 @@ class ShareController < ApplicationController
     def index
         referral_id = 123456789 + current_user.id
         @referral_code = referral_id.to_s(36) 
-        # not sure why share_code throws an error here - need to migrate db maybe?
-        # current_user.share_code = @referral_code
+        current_user.share_code = @referral_code
         current_user.save!
     end
 end
--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
+  test "does the user object exist" do
+  if @user.exists?
+    assert true
+  end
+end
 end
