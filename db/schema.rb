87f009f828a5b9cfbd5d313236f8d33999f5e7ac
Emily Wright <emily@emilyville.com>
2012-05-29 11:11:42 -0700

    get schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(:version => 20120523205440) do
     t.integer  "user_id"
     t.integer  "task_id"
     t.boolean  "completed"
-    t.date     "remind"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
