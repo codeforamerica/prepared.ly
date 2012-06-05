503155b5a5aae929d180a95f14833f46302fe966
Joe Merante <merante.joe@gmail.com>
2012-06-05 14:15:35 -0700

    update share page to require authentication

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -1,4 +1,6 @@
 class ShareController < ApplicationController
+    before_filter :authenticate_user!
+    
     def index
     # user's share code is generated in the create action of registrations_controller.rb for regular signups
     # or in the users_controller.rb for oauth users so it's saved to the user before the welcome email goes
