66c31a1cfb92a29f226e260e3aa705aa6b5012e8
Joe Merante <merante.joe@gmail.com>
2012-07-02 17:10:58 -0700

    fix migration so it acts on most recent state of staging db

--- db/migrate/20120702203949_add_reminder_type_to_messages.rb
+++ db/migrate/20120702203949_add_reminder_type_to_messages.rb
@@ -1,7 +1,5 @@
 class AddReminderTypeToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :reminder_type, :string
-    # column name 'type' is reserved 
-    remove_column :messages, :type
   end
 end
