2e5e1061c1f2dd72933e6d28bddce9cc26b784b5
Joe Merante <merante.joe@gmail.com>
2012-07-31 16:19:50 -0700

    add kiss metrics js for recording specific events

--- app/assets/javascripts/kiss.js
+++ app/assets/javascripts/kiss.js
@@ -10,3 +10,19 @@ function _kms(u){
 }
 _kms('//i.kissmetrics.com/i.js');
 _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+
+_kmq.push(['record', 'Contacts expert']);
+_kmq.push(['record', 'Adds comment']);
+_kmq.push(['record', 'Schedules task']);
+_kmq.push(['record', 'Skip task']);
+_kmq.push(['record', 'Completes task']);
+_kmq.push(['record', 'Load map']);
+_kmq.push(['record', 'Auth Yahoo']);
+_kmq.push(['record', 'Auth Facebook']);
+_kmq.push(['record', 'Auth Google']);
+_kmq.push(['record', 'Auth Twitter']);
+_kmq.push(['record', 'Clicks Sign Up']);
+_kmq.push(['record', 'Clicks Sign In']);
+_kmq.push(['record', 'Views login modal']);
+_kmq.push(['record', 'Clicks login link']);
+_kmq.push(['record', 'Visit homepage']);
