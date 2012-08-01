675bff2a03c5ca0e3bac1df40088c5d0156bcfb4
Joe Merante <merante.joe@gmail.com>
2012-07-31 17:24:17 -0700

    update share controller before calling object in share view, thanks staging\!

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -4,6 +4,7 @@ class ShareController < ApplicationController
     def index
     # user's share code is generated in the create action of registrations_controller.rb for regular signups
     # or in the users_controller.rb for oauth users so it's saved to the user before the welcome email goes
+	    @user = current_user
 	    @share_code = current_user.share_code
     end
 end
