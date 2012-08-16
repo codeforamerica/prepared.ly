1de22e318c167dbce39020659d7235a35c712786
Joe Merante <merante.joe@gmail.com>
2012-08-15 18:03:08 -0700

    application.js comments troubleshooting ie8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,19 +30,34 @@
 
 //add dashboard tab href to url in browser address bar
 $('#sidenav a').click(function(e){
+  console.log("a link in the sidenav has been clicked on");
+  e.stopPropagation();
+  e.preventDefault();
   var href = $(this).attr('href');
   window.location = href;
+  console.log("href:");
+  console.log(href);
+  console.log("window.location:");
+  console.log(window.location);
 });
 
 $(document).ready(function(){
-  //load map-section in dashboard by default on page load for authenticated user
+  console.log("sidenav.length (expecting > 0): " + $("#sidenav").length);
+  console.log("window.location.hash (expecting empty): " + window.location.hash);
+  console.log($("#sidenav"));
+
+  // By default, load #map-section in user dashboard
   if(($("#sidenav").length > 0) && (!window.location.hash)) {
+    console.log("sidenav.length is > 0 and window.location.hash is empty");
     window.location.hash = "#map-section";
-    // permalink for dashboard tabs for authenticated user
-  } else {
+  }
+  // Handle permalinks to non-#map-section tabs in user dashboard
+  else {
+    console.log("either sidenav.length was 0 or window.location.hash is true");
     var match = window.location.hash;
     $('#sidenav a[href="' + match + '"]').tab('show');
   };
+
   //use browser forward/back buttons to navigate dashboard tabs
   $(window).bind('hashchange', function(){
     var active_tab = window.location.hash;
