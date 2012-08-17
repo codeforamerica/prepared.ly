f7c4ad8c9d8733e653256000d8d2cac8762ce061
Joe Merante <merante.joe@gmail.com>
2012-08-17 15:40:27 -0700

    add back typekit and gsfn now that IE errors fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -129,17 +129,17 @@ var is_ssl = ("https:" == document.location.protocol);
 var asset_host = is_ssl ? "https://d3rdqalhjaisuu.cloudfront.net/" : "http://d3rdqalhjaisuu.cloudfront.net/";
 document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E"));
 
-// var feedback_widget_options = {};
-//   feedback_widget_options.display = "overlay";
-//   feedback_widget_options.company = "code_for_america";
-//   feedback_widget_options.placement = "right";
-//   feedback_widget_options.color = "#333333";
-//   feedback_widget_options.style = "question";
-//   feedback_widget_options.product = "code_for_america_preparedly";
-//   feedback_widget_options.tag = "feedback";
-//   feedback_widget_options.custom_css = "feedback.css";
-//   feedback_widget_options.limit = "3";
-// var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
+var feedback_widget_options = {};
+  feedback_widget_options.display = "overlay";
+  feedback_widget_options.company = "code_for_america";
+  feedback_widget_options.placement = "right";
+  feedback_widget_options.color = "#333333";
+  feedback_widget_options.style = "question";
+  feedback_widget_options.product = "code_for_america_preparedly";
+  feedback_widget_options.tag = "feedback";
+  feedback_widget_options.custom_css = "feedback.css";
+  feedback_widget_options.limit = "3";
+var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
 
 // google analytics
 var _gaq = _gaq || [];
