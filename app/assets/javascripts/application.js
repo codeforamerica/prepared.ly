19ef2fa491f1671df5cf52eddaaeb6f8c87f272e
Joe Merante <merante.joe@gmail.com>
2012-08-21 17:17:35 -0700

    add missing word on alert page, move google analytics code to head of layout files to ensure proper loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -141,13 +141,4 @@ var feedback_widget_options = {};
   feedback_widget_options.limit = "3";
 var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
 
-// google analytics
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-30366897-1']);
-  _gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();   
+// google analytics code moved to application.html.erb to ensure loading in head tag  
