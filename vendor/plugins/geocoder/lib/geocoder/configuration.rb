3a8a4093a32a5b9b81d167b95809e7265b0be695
Joe Merante <merante.joe@gmail.com>
2012-09-04 12:12:30 -0700

    before taking step noted in previous commit, try using :geocoder_ca for lookup since it works ok on localhost

--- vendor/plugins/geocoder/lib/geocoder/configuration.rb
+++ vendor/plugins/geocoder/lib/geocoder/configuration.rb
@@ -7,7 +7,7 @@ module Geocoder
         [:timeout, 3],
 
         # name of geocoding service (symbol)
-        [:lookup, :google],
+        [:lookup, :geocoder_ca],
 
         # ISO-639 language code
         [:language, :en],
